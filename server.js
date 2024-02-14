379731a22122a186532427b0b2a10732b7cdd914
Thong Chau <80946011+TChauFlamingHeart@users.noreply.github.com>
2024-02-14 00:30:02 -0800

    gitignore

--- server.js
+++ server.js
@@ -5,6 +5,70 @@ const fs = require('fs');
 const app = express();
 var PORT = process.env.PORT || 3000;
 const server = app.listen(PORT); //tells to host server on localhost:3000
+const mysql = require('mysql2');
+
+
+
+
+//___________testing database connection___________________________________________________
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '24$+vY!#Fjk-3',
+    database: 'testgrocer'
+  });
+
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      return;
+    }
+    console.log('Connected to MySQL database');
+  });
+
+
+//testing database operations
+const inputData = {
+    username: 'example_user0',
+    email: 'user098@example.com',
+    password: 'rand0mP4ss'
+};
+
+// // insert object into database
+// connection.query('INSERT INTO testgrocer.users SET ?', inputData, (err, results) => {
+//     if (err) {
+//       console.error('Error inserting data into MySQL database:', err);
+//       return;
+//     }
+//     console.log('Data inserted successfully:', results);
+//   });
+
+//retrieve information and format
+connection.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      console.error('Error querying MySQL database:', err);
+      return;
+    }
+
+    // Display user information in the terminal
+    console.log('User Information:');
+    results.forEach((user) => {
+      console.log(`Username: ${user.username}, Email: ${user.email}`);
+    })
+});
+
+// Handle errors
+connection.on('error', (err) => {
+    console.error('MySQL database error:', err);
+  });
+  
+// Close the database connection when the Node.js process exits
+process.on('exit', () => {
+    connection.end();
+  });
+
+// //____________________________________________________
 
 
 //Playing variables:
@@ -49,9 +113,10 @@ io.on('connection', (socket) => {
     // Handle 'sql_query' event
     socket.on('sql_query', async () => {
         try {
-            const [rows, fields] = await db.promise().query("SELECT * FROM users");
-            console.log(rows);
+            const [rows, fields] = await db.promise().query("SELECT username,email FROM users");
+            console.log("here are the users: ", rows);
             // Emit the fetched data back to the client
+            // socket.emit('hello_post', 'Post these are the users');
             socket.emit('sql_result', rows);
         } catch (err) {
             console.error(err.message, "sql_query");
@@ -60,3 +125,8 @@ io.on('connection', (socket) => {
         }
     })
 })
+
+
+
+
+
